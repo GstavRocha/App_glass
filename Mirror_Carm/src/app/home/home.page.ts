2eecc81f83123b139e0de755e511fbb1d04ff32d
Luiz Gustavo da Rocha Ferreira <20211148060008@ifrn.local>
2023-06-19 17:25:34 -0300

    atividade3

--- Mirror_Carm/src/app/home/home.page.ts
+++ Mirror_Carm/src/app/home/home.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CameraPreview, CameraPreviewOptions } from '@capacitor-community/camera-preview';
 
+const cameraPreviewOptions: CameraPreviewOptions = {
+  position: 'rear',
+  height: 1920,
+  width: 1080
+};
+CameraPreview.start(cameraPreviewOptions);
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  cameraPreviewOptions: CameraPreviewOptions ={
+    position: 'front',
+    height: 1920,
+    width: 1080
 
+  }
   constructor() {}
 
 }
