18cffb8d78cc673aedee57f1b9290de91e238c1d
Luiz Gustavo da Rocha Ferreira <20211148060008@ifrn.local>
2023-06-19 17:03:54 -0300

    atividade

---
+++ Mirror_Carm/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mirror_Carm',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
